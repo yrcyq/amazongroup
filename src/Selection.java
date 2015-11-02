f03b8789dd62f2a67e275479ed59f5f4d3b4ea30
yrcyq <yrc1234@gmail.com>
2015-11-02 15:39:15 -0800

    add question 2 brute force solution

---
+++ src/Selection.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Selection {
+	public static List<Content> select(int time, Map<Integer, List<Content>> schedules, Map<Integer, Double> areaWeight){
+		List<Content> result = new ArrayList<>();
+		findQualifiedContent(time, schedules);
+		bruteForce(schedules, areaWeight, new HashSet<Integer>(), new Double(0), new ArrayList<Content>(), result, 0, 0);
+		return result;
+	}
+	
+	private static void findQualifiedContent(int time, Map<Integer, List<Content>> schedules) {
+		// TODO Auto-generated method stub
+		if(time < 1 || schedules.isEmpty())
+			return ;
+		for(int i : schedules.keySet()){
+			List<Content> schedule = schedules.get(i);
+			for(Content c : schedule){
+				if(time < c.getStart() || time > c.getEnd())
+					schedule.remove(c);
+			}
+		}
+	}
+
+	private static void bruteForce(Map<Integer, List<Content>> schedules, Map<Integer, Double> areaWeight, 
+			Set<Integer> visited, Double globalSum, List<Content> currentSelction, List<Content> result, 
+			int idx, double currentSum){
+		if(idx == schedules.size()){
+			if(currentSum > globalSum.doubleValue()){
+				globalSum  = new Double(currentSum);
+				result = new ArrayList<>(currentSelction);
+			}
+			return;
+		}
+		List<Content> schedule = schedules.get(idx);
+		for(Content c : schedule){
+			if(visited.contains(c.getId()))
+				continue;
+			visited.add(c.getId());
+			currentSelction.add(c);
+			bruteForce(schedules, areaWeight, visited, globalSum, currentSelction, result, idx+1, currentSum+c.getContentWeight()*areaWeight.get(idx));
+			visited.remove(c.getId());
+			currentSelction.remove(currentSelction.size()-1);
+		}
+	}
+}
