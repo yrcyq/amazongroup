0b37a987d183897d479bd68410a7bb8d8c114ad9
yrcyq <yrc1234@gmail.com>
2015-11-02 16:25:29 -0800

    question 2: greedy solution

---
+++ src/SelectionGreedy.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class SelectionGreedy {
+	public List<Content> select(Map<Integer, Double> areaWeight, Map<Integer, List<Content>> schedules, 
+			int time){
+		
+	}
+}
